184e213f49c815794665bd7460b50391b55a8c15
Taylor Lewis <tlew@Taylors-MacBook-Pro.local>
2019-01-07 13:51:38 -0600

    update 3

---
+++ photo.js
@@ -0,0 +1,15 @@
+// slideshow js
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("item15");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
